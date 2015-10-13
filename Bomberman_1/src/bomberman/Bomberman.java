8bdbf4adacdab875817abe2cefb3218d0d43b864
JeffKain <Heartless275>
2015-10-13 09:59:37 +0200

    Teams added

--- Bomberman_1/src/bomberman/Bomberman.java
+++ Bomberman_1/src/bomberman/Bomberman.java
@@ -5,13 +5,17 @@
  */
 package bomberman;
 
+import Game.Team;
+import java.awt.Rectangle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.fills.GradientFill;
 /**
  *
  * @author jeffrey
@@ -21,22 +25,50 @@ public class Bomberman extends BasicGame{
     /**
      * @param args the command line arguments
      */
+    
+         private Team team1;
+         private Team team2;
         // TODO code application logic here
-        	public Bomberman(String gamename)
+        public Bomberman(String gamename)
 	{
 		super(gamename);
 	}
 
 	@Override
-	public void init(GameContainer gc) throws SlickException {}
+	public void init(GameContainer gc) throws SlickException {
+        team1 = new Team();
+        team2 = new Team();
+        }
 
 	@Override
-	public void update(GameContainer gc, int i) throws SlickException {}
+	public void update(GameContainer gc, int i) throws SlickException {
+           //team1.damage();
+        }
 
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
-		g.drawString("Howdy!", 100, 100);
+            
+        float healthScale = team1.currentHealth() / team1.maxHealth();
+        float healthScale2 = team2.currentHealth() / team2.maxHealth();
+        
+        g.setColor(Color.white);
+        g.drawString("Team 1 HP: ", 10.0f, 30.0f);
+        g.setColor(Color.green);
+        g.fillRect(100.0f, 30.0f, 250f * healthScale, 20.0f);
+        
+
+        g.setColor(Color.white);
+        g.drawString("Team 2 HP: ", 760.0f, 30.0f);
+        g.setColor(Color.green);
+        g.fillRect(850.0f, 30.0f, 250f * healthScale2, 20.0f);
+        
+	//g.drawString("Howdy!", 100, 100);
+//        g.setColor(Color.green);
+//        g.fillRect(20.0f, 10.0f, 300.0f, 20.0f);
+//        
+//        g.setColor(Color.blue);
+//        g.fillRect(20.0f, 10.0f, team1.maxHealth() / team1.currentHealth() * 300.0f, 20.0f);
 	}
 
 	public static void main(String[] args)
@@ -45,7 +77,7 @@ public class Bomberman extends BasicGame{
 		{
 			AppGameContainer appgc;
 			appgc = new AppGameContainer(new Bomberman("Simple Slick Game"));
-			appgc.setDisplayMode(1960, 1000, false);
+			appgc.setDisplayMode(1080, 760, false);
 			appgc.start();
 		}
 		catch (SlickException ex)
