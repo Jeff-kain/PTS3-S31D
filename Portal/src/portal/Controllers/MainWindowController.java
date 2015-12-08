1abe1dd837b28207e9a0077f6ea2c7f0b17d57d8
teveve <t.vervelde@student.fontys.nl>
2015-12-08 11:09:19 +0100

    Added the files to the correct packages.

--- Portal/src/portal/Controllers/MainWindowController.java
+++ Portal/src/portal/Controllers/MainWindowController.java
@@ -22,6 +22,6 @@ public class MainWindowController {
         observableGames = FXCollections.observableArrayList();
         observableGames.add(bomberman);
 
-        lvwGame.setItems(observableGames);
+        //lvwGame.setItems(observableGames);
     }
 }
