2cad0e0914f60ead8eaa917460e7c83481dfd95d
Kappa013 <ferhat.yu@hotmail.com>
2015-12-14 12:59:37 +0100

    Trying to fix multiple player movement

--- Bomberman_1/src/Multiplayer/HostServer.java
+++ Bomberman_1/src/Multiplayer/HostServer.java
@@ -103,7 +103,8 @@ public class HostServer extends UnicastRemoteObject implements IRemoteHost {
     @Override
     public void movep2c(int direction, float x, float y) throws RemoteException {
         direction = translate(direction);
-        ((Player) (Game.getInstance().getAllPlayers().get(0))).moveremote(direction, x, y);
+        Game.getInstance().getPlayer1().get(0).moveremote(direction, x, y);
+        Game.getInstance().getPlayer2().get(0).moveremote(direction, x, y);
     }
 
     public int translate(int direction) {
